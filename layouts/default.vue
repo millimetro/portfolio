87d5ce38fc9dd8687cb14f65d29094abc0a12b3b
millimetro <millimetro.art@gmail.com>
2022-09-03 15:53:30 +0200

    fixed cursor and gh pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,15 +23,12 @@ export default {
   //   });
   // },
 
-  data() {
+  mounted() {
     const cursor = document.querySelector(".cursor");
     document.addEventListener("mousemove", (e) => {
       cursor.style.left = e.pageX + "px";
       cursor.style.top = e.pageY + "px";
     });
-    return {
-      cursor,
-    };
   },
 };
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // router:  {
-  //   base: ''
-  // },
+
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
